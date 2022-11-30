314b18d4ace2cec40f786634938ddaf07ccca841
Orlando Aguilar <orlando.aguilar@ucrso.info>
2022-11-30 17:56:24 -0600

    Working with the server start Automatic

---
+++ test/examen2/AguinaldoTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author iOrlando
+ */
+public class AguinaldoTest {
+    
+    public AguinaldoTest() {
+    }
+
+    @Test
+    public void testFindbonus() {
+    }
+
+    @Test
+    public void testBonus() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+    
+}
